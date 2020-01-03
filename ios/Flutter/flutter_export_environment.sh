e5a0d83143a54f936cf3ab830538f17f524d2306
Tiago Silva <tsilvasantos38@gmail.com>
2020-01-03 18:16:44 -0300

    .

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/tiago/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/tiago/Documents/Projetos/Personal_flow/projeto_integrador_i_IESB"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\tigos\Documents\GitHub\projeto_integrador_i_IESB"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/tiago/development/flutter/bin/cache/artifacts/engine/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
