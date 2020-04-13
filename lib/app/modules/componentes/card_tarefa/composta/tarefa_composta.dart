df9b4cad877476108aee80ecf9f8bed533c756ac
Tiago Silva <44373482+tigosante@users.noreply.github.com>
2020-04-13 15:09:31 -0300

    ajuste.

--- lib/app/modules/componentes/card_tarefa/composta/tarefa_composta.dart
+++ lib/app/modules/componentes/card_tarefa/composta/tarefa_composta.dart
@@ -11,35 +11,30 @@ class TarefaComposta extends StatelessWidget {
     double tamanhoTela = MediaQuery.of(context).size.width;
 
     return Card(
-      elevation: 1.5,
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(tamanhoTela * 0.05)),
-      child: ExpansionTile(
-        title: Padding(
-          padding: EdgeInsets.only(
-              top: tamanhoTela * 0.03, left: tamanhoTela * 0.018),
-          child: Text("Título da composta.",
-              style: TextStyle(
-                  fontSize: tamanhoTela * 0.045, fontWeight: FontWeight.bold)),
-        ),
-        subtitle: Column(
-          children: <Widget>[
-            Container(width: tamanhoTela, child: DataHora()),
-            Padding(
-              padding: EdgeInsets.only(left: tamanhoTela * 0.02),
-              child: Container(
-                  width: tamanhoTela,
-                  height: tamanhoTela * 0.06,
-                  child: BarraProgresso(tamanhoTela: tamanhoTela)),
-            ),
-          ],
-        ),
-        children: <Widget>[
-          BodyComposta(),
-          BotoesTarefa(tamanhoTela: tamanhoTela),
-        ],
-      ),
-    );
+        elevation: 1.5,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(tamanhoTela * 0.05)),
+        child: ExpansionTile(
+            title: Padding(
+                padding: EdgeInsets.only(
+                    top: tamanhoTela * 0.03, left: tamanhoTela * 0.018),
+                child: Text("Título da composta.",
+                    style: TextStyle(
+                        fontSize: tamanhoTela * 0.045,
+                        fontWeight: FontWeight.bold))),
+            subtitle: Column(children: <Widget>[
+              Container(width: tamanhoTela, child: DataHora()),
+              Padding(
+                  padding: EdgeInsets.only(left: tamanhoTela * 0.02),
+                  child: Container(
+                      width: tamanhoTela,
+                      height: tamanhoTela * 0.06,
+                      child: BarraProgresso(tamanhoTela: tamanhoTela))),
+            ]),
+            children: <Widget>[
+              BodyComposta(),
+              BotoesTarefa(tamanhoTela: tamanhoTela)
+            ]));
   }
 }
