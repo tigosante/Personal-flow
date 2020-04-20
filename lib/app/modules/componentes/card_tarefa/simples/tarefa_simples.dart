56cec99d18c8d4d1526aa3c6899f7b711b855e2b
Tiago Silva <44373482+tigosante@users.noreply.github.com>
2020-04-20 04:41:54 -0300

    ajuste.

--- lib/app/modules/componentes/card_tarefa/simples/tarefa_simples.dart
+++ lib/app/modules/componentes/card_tarefa/simples/tarefa_simples.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:personalflow/app/model/simples/model.dart';
 import 'package:personalflow/app/modules/componentes/data_hora/data_hora.dart';
 import 'package:personalflow/app/modules/home/pages/simples/simples_controller.dart';
@@ -19,24 +20,27 @@ class TarefaSimples extends StatelessWidget {
         clipBehavior: Clip.antiAliasWithSaveLayer,
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(tamanhoTela * 0.05)),
-        child: ListTile(
-            onTap: () {
-              controller.dialogTarefa(context, tarefas[index], tamanhoTela);
-            },
-            title: Padding(
-                padding: EdgeInsets.only(
-                    top: tamanhoTela * 0.03, left: tamanhoTela * 0.015),
-                child: Text(tarefas[index].title,
-                    style: TextStyle(
-                        fontSize: tamanhoTela * 0.045,
-                        fontWeight: FontWeight.bold))),
-            subtitle: DataHora(),
-            trailing: IconButton(
-                icon: Icon(
-                    tarefas[index].check
-                        ? Icons.check
-                        : Icons.radio_button_unchecked,
-                    color: Colors.teal[700]),
-                onPressed: tarefas[index].updateCheck)));
+        child: Slidable(
+          actionPane: null,
+          child: ListTile(
+              onTap: () {
+                controller.dialogTarefa(context, tarefas[index], tamanhoTela);
+              },
+              title: Padding(
+                  padding: EdgeInsets.only(
+                      top: tamanhoTela * 0.03, left: tamanhoTela * 0.015),
+                  child: Text(tarefas[index].title,
+                      style: TextStyle(
+                          fontSize: tamanhoTela * 0.045,
+                          fontWeight: FontWeight.bold))),
+              subtitle: DataHora(),
+              trailing: IconButton(
+                  icon: Icon(
+                      tarefas[index].check
+                          ? Icons.check
+                          : Icons.radio_button_unchecked,
+                      color: Colors.teal[700]),
+                  onPressed: tarefas[index].updateCheck)),
+        ));
   }
 }
