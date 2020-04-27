752a89c7b2cd9105d287e70106bfe5d0eba4e136
Tiago Silva <44373482+tigosante@users.noreply.github.com>
2020-04-27 03:23:36 -0300

    ajustes gerais;

---
+++ lib/core/model/login/login_repository.dart
@@ -0,0 +1,15 @@
+import 'package:personalflow/core/model/login/login_repository_interface.dart';
+
+class LoginRepository implements ILoginRepository {
+  @override
+  Future getGoogleLogin() {
+    // TODO: implement getGoogleLogin
+    return null;
+  }
+
+  @override
+  Future getUser() {
+    // TODO: implement getUser
+    return null;
+  }
+}
---
+++ lib/core/model/login/login_repository_interface.dart
@@ -0,0 +1,4 @@
+abstract class ILoginRepository {
+  Future getGoogleLogin();
+  Future getUser();
+}
