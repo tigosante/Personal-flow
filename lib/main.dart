06b4b55e6893174ef08d819ea23f12642edddaad
tigosante <tsilvasantos38@gmail.com>
2020-08-08 14:01:11 -0300

    limpeza

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:personalflow/app/modules/app_module.dart';
 
-void main() => runApp(ModularApp(module: AppModule()));
