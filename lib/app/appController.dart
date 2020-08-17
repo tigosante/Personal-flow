1acd6a9db7770a729ea0e6231b99fbc1bccea724
Tiago Silva <44373482+tigosante@users.noreply.github.com>
2020-08-17 15:31:30 -0300

    .

--- lib/app/appController.dart
+++ lib/app/appController.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 
 part 'appController.g.dart';
@@ -5,5 +6,6 @@ part 'appController.g.dart';
 class AppController = _AppControllerBase with _$AppController;
 
 abstract class _AppControllerBase with Store {
-  int value = 0;
+  String initialRoute = "/";
+  ThemeData themeData = ThemeData(primarySwatch: Colors.red);
 }
--- lib/app/appWidget.dart
+++ lib/app/appWidget.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:personal_flow/app/appController.dart';
 
 class AppWidget extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Personal Flow',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        initialRoute: '/',
-        navigatorKey: Modular.navigatorKey,
-        onGenerateRoute: Modular.generateRoute,
-        debugShowCheckedModeBanner: false,
-      );
+  Widget build(BuildContext context) {
+    final controller = Modular.get<AppController>();
+
+    return MaterialApp(
+      title: 'Personal Flow',
+      theme: controller.themeData,
+      initialRoute: controller.initialRoute,
+      navigatorKey: Modular.navigatorKey,
+      onGenerateRoute: Modular.generateRoute,
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
